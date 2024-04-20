a84af39af7287c0472399f7a7acbd80c3748e850
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-04-19 22:17:10 -0300

    completed activity

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
+const form = document.querySelector('[data-js="form-element"]');
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
-function removerLinha(botao) {
-    var row = botao.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-}
+  const data = document.querySelectorAll('.usuario');
+  const table = document.querySelector('[data-js="data-tabela"]');
+
+  const value = `<tr data-js="data-tr">
+                    <td>${data[0].value}</td>
+                    <td>${data[1].value}</td>
+                    <td>${data[2].value}</td>
+                    <td class="edit">
+                      <input type="button" data-js="delete" class="botao" value="X">
+                      <input type="button" data-js="edit" class="botao" value="✎">
+                    </td>
+                  </tr>`;
+
+  table.insertAdjacentHTML('beforeend', value);
+
+  form.reset();
+
+  const deleteElements = document.querySelectorAll('[data-js="delete"]');
+  const editElements = document.querySelectorAll('[data-js="edit"]');
+
+  deleteElements.forEach(deleteElement => {
+    deleteElement.addEventListener("click", () => {
+      deleteElement.closest('[data-js="data-tr"]').remove();
+    });
+  });
+
+  editElements.forEach(editElement => {
+    editElement.addEventListener("click", () => {
+      editElement.closest('[data-js="data-tr"]').remove();
+      const value = `<tr data-js="data-tr">
+      <td>${data[0].value}</td>
+      <td>${data[1].value}</td>
+      <td>${data[2].value}</td>
+      <td class="edit">
+        <input type="button" data-js="delete" class="botao" value="X">
+        <input type="button" data-js="edit" class="botao" value="✎">
+      </td>
+    </tr>`;
+
+    table.insertAdjacentHTML('beforeend', value);
+
+    form.reset();
+    });
+  });
+});
+
